0a8de127a0ce37694305bc15ddfa8fe34eeab7d2
Leo <leozockt8210@gmail.com>
2026-03-08 18:59:43 +0100

    Final

--- src/main/java/de/SurvivalChallengesPlugin/general/challenges/events/MobDuplicator.java
+++ src/main/java/de/SurvivalChallengesPlugin/general/challenges/events/MobDuplicator.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
@@ -22,9 +23,10 @@ public class MobDuplicator implements Listener {
 
     @EventHandler
     public void onMobSpawn(CreatureSpawnEvent event) {
-        de.SurvivalChallengesPlugin.timer.utils.Timer timer = SurvivalChallengesPlugin.getInstance().getTimer();
+        var timer = SurvivalChallengesPlugin.getInstance().getTimer();
         if (!timer.isRunning()) return;
-        Challenges challenges = SurvivalChallengesPlugin.getInstance().getChallenges();
+
+        var challenges = SurvivalChallengesPlugin.getInstance().getChallenges();
         if (challenges.isActive(Challenges.Challenge.MOB_DUPLICATOR)) {
             if(ClearMobs) {
                 event.setCancelled(true);
@@ -37,41 +39,46 @@ public class MobDuplicator implements Listener {
 
     @EventHandler
     public void onMobDies(EntityDeathEvent event) {
-        de.SurvivalChallengesPlugin.timer.utils.Timer timer = SurvivalChallengesPlugin.getInstance().getTimer();
+        var timer = SurvivalChallengesPlugin.getInstance().getTimer();
         if (!timer.isRunning()) return;
-        Challenges challenges = SurvivalChallengesPlugin.getInstance().getChallenges();
-        if (challenges.isActive(Challenges.Challenge.MOB_DUPLICATOR)) {
-            int count = event.getEntity().getWorld().getLivingEntities().size();
-            if (count >= 1500){
-                ClearMobs = true;
-                for (LivingEntity entity : event.getEntity().getWorld().getLivingEntities()) {
-                    if (entity instanceof Player) continue;
-                    if (!entity.hasMetadata("duplication")) continue;
-                    for (MetadataValue value : entity.getMetadata("duplication")) {
-                        if (value.getOwningPlugin() != SurvivalChallengesPlugin.getInstance()) continue;
-                        int duplication = value.asInt();
-                        if (duplication >= 16) {
-                            entity.remove();
-                        }
+        var challenges = SurvivalChallengesPlugin.getInstance().getChallenges();
+        if (!challenges.isActive(Challenges.Challenge.MOB_DUPLICATOR)) return;
+        int count = event.getEntity().getWorld().getLivingEntities().size();
+        if (count >= 1500) {
+            ClearMobs = true;
+            for (LivingEntity entity : event.getEntity().getWorld().getLivingEntities()) {
+                if (entity instanceof Player) continue;
+                if (!entity.hasMetadata("duplication")) continue;
+                for (MetadataValue value : entity.getMetadata("duplication")) {
+                    if (value.getOwningPlugin() != SurvivalChallengesPlugin.getInstance()) continue;
+                    int duplication = value.asInt();
+                    if (duplication >= 16) {
+                        entity.remove();
                     }
                 }
-                for(Player player : Bukkit.getOnlinePlayers())
-                    player.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "MobDuplicator" + ChatColor.DARK_GRAY + "] " + ChatColor.RED + "Removed required mobs to prevent lag (" + count + " entities)");
-                Bukkit.getScheduler().runTaskLater(SurvivalChallengesPlugin.getInstance(), () -> ClearMobs = false,1L);
             }
-            if (event.getEntity().getKiller() == null) return;
-            if(ClearMobs) return;
-            if (event.getEntity().hasMetadata("duplication")) {
-                List<MetadataValue> values = event.getEntity().getMetadata("duplication");
-                for (MetadataValue value : values) {
-                    if (value.getOwningPlugin() == SurvivalChallengesPlugin.getInstance()) {
-                        int duplication = value.asInt();
-                        duplication = duplication * 2;
-                        for (int i = 1; i <= duplication; i++) {
-                            Entity newMob = event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), event.getEntity().getType());
-                            newMob.setMetadata("duplication", new FixedMetadataValue(SurvivalChallengesPlugin.getInstance(), duplication));
-                        }
-                    }
+            for(Player player : Bukkit.getOnlinePlayers())
+                player.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "MobDuplicator" + ChatColor.DARK_GRAY + "] " + ChatColor.RED + "Removed required mobs to prevent lag (" + count + " entities)");
+            Bukkit.getScheduler().runTaskLater(SurvivalChallengesPlugin.getInstance(), () -> ClearMobs = false, 1L);
+        }
+
+        if(ClearMobs) return;
+        if (!event.getEntity().hasMetadata("duplication")) {
+            event.getEntity().setMetadata("duplication", new FixedMetadataValue(SurvivalChallengesPlugin.getInstance(), 1));
+        }
+        boolean playerInvolved = false;
+        if (event.getEntity().getKiller() != null) playerInvolved = true;
+        else if (event.getEntity().getLastDamageCause() instanceof EntityDamageByEntityEvent e) {
+            if (e.getDamager() instanceof Player) playerInvolved = true;
+        }
+        if (!playerInvolved) return;
+        List<MetadataValue> values = event.getEntity().getMetadata("duplication");
+        for (MetadataValue value : values) {
+            if (value.getOwningPlugin() == SurvivalChallengesPlugin.getInstance()) {
+                int duplication = value.asInt() * 2;
+                for (int i = 0; i < duplication; i++) {
+                    Entity newMob = event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), event.getEntity().getType());
+                    newMob.setMetadata("duplication", new FixedMetadataValue(SurvivalChallengesPlugin.getInstance(), duplication));
                 }
             }
         }
